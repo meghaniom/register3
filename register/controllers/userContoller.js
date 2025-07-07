d8f00b4b50f6f599489ccda3f26bdcb356644e21
om meghani <meghanio264@gmail.com>
2025-07-07 11:06:30 +0530

    code change

---
+++ register/controllers/userContoller.js
@@ -0,0 +1,54 @@
+const User = require('../models/usermodel');
+const bcrypt = require("bcrypt");
+
+
+ exports.register = async (req, res) => {
+    try {
+        const { email, password, username } = req.body;
+
+        
+        const existingUser = await User.findOne({
+            $or: [{ email }, { username }]
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists.' });
+        }
+
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+       
+        const newUser = await User.create({
+            email,
+            password: hashedPassword,
+            username
+        });
+
+        res.status(200).json({ message: 'User created successfully.' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+ exports.login  = async(req, res) => {
+    try {
+        const { email , password } = req.body;
+         const loginUser = await User.findOne({ email});
+
+         if (!loginUser) {
+            return res.status(400).json({ message : "Invalid email the email are not registered"})
+         }
+         const isPasswordValid = await bcrypt.compare (password, loginUser.password);
+          if (!isPasswordValid) {
+             return res.status(400).json({ message : "Invalid password"})
+          }
+           res.status(200).json({ message : "Login successfully"})
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message : "Internal server error"});
+    }
+ }
---
+++ register/routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const useController = require("../controllers/userContoller")
+
+
+
+ router.post('/signup', useController.register)
+    router.post('/login', useController.login)
+
+
+ module.exports = router;
